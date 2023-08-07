354491777af22d6d28d9df451aa4c06ff64fb536
LosDelaro <cdelarosa25@yahoo.com>
2023-08-07 10:51:50 -0500

    Completed HW 10

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -17,19 +17,31 @@ app = Flask(__name__)
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
+
+
+
 #################################################
 # Flask Routes
 #################################################
+
 @app.route("/")
 def welcome():
-    """List all available api routes."""
-    return (
-        f"""<h1>Available Routes:<h1/><br/>"""
-    )
+    # """List all available api routes."""
+    return(
+        
+        f"""<h1>Available Routes:<h1/><br/>
+        <a href= "/api/v1.0/precipitation">/api/v1.0/precipitation</a><br>
+        <a href= "/api/v1.0/stations">/api/v1.0/stations</a><br>
+        <a href= "/api/v1.0/tobs">/api/v1.0/tobs</a><br>
+        <a href= "/api/v1.0/2016-08-23">/api/v1.0/2016-08-23</a><br>
+        <a href= "/api/v1.0/2016-08-23/2016-09-23">/api/v1.0/2016-08-23/2016-09-23</a><br>
+        """
+    )   
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Get Precipitation"""
+    # """Get Precipitation"""
     query = text("""
                 SELECT
                    date,
@@ -45,6 +57,96 @@ def precipitation():
     data = json.loads(df.to_json(orient="records"))
     return(data)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    # """Get station"""
+    query = text("""
+                SELECT
+                   station,
+                   count(*)as num_obs
+                FROM
+                    measurement
+                group by 
+                    station 
+                order by
+                    num_obs desc;
+            """)
+
+    df = pd.read_sql(query, engine)
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # """Get temperature observation (TOBS) data"""
+    query = text("""
+                SELECT
+                   date,
+                   station,
+                   tobs as temperature
+                FROM
+                    measurement
+                where 
+                    date >= '2016-08-23'
+                    and station = 'USC00519281'
+                 Order by
+                        date asc;
+            """)
+    
+
+
+
+    df = pd.read_sql(query, engine)
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+
+
+@app.route("/api/v1.0/<start>")
+def temperature_start(start):
+    # """Get temperature observation (TOBS) data"""
+    query = text(f"""
+                SELECT
+                   station,
+                   min(tobs) as tmin,
+                   avg(tobs) as tmean,
+                   max(tobs) as tmas                 
+                FROM
+                    measurement
+                where 
+                    date >= '{start}';
+            """)
+    
+
+    
+
+    df = pd.read_sql(query, engine)
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_start_end(start,end):
+    # """Get temperature observation (TOBS) data"""
+    query = text(f"""
+                SELECT
+                   station,
+                   min(tobs) as tmin,
+                   avg(tobs) as tmean,
+                   max(tobs) as tmas                 
+                FROM
+                    measurement
+                where 
+                    date >= '{start}'
+                    and date <='{end}';
+            """)
+    
+
+    
+
+    df = pd.read_sql(query, engine)
+    data = json.loads(df.to_json(orient="records"))
+    return(data)
 
 
 #run the website
